3ed7876a43d3a39149d084a3824056e67a897941
oliver092377 <omayta@unsa.edu.pe>
2022-05-07 16:25:04 -0500

    Mi presentacion

--- Bienvenida.java
+++ Bienvenida.java
@@ -1,7 +1,7 @@
 public class Bienvenida{
     public static void main(String args[]) {
     
-        //Mi primer "Hola mundo" en java
+        //Mi bienvenida al curso de EDA
         System.out.println ("Bienvenido al curso de EDA");
 
     }
---
+++ Presentacion.java
@@ -0,0 +1,8 @@
+public class Presentacion{
+    public static void main(String args[]) {
+    
+        //Mi presentación
+        System.out.println ("Mi nombre es Oliver Mayta :)");
+
+    }
+}
