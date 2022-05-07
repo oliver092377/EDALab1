dd5c8fd9582375433f562dffcfd90bbbe0ccf229
oliver092377 <omayta@unsa.edu.pe>
2022-05-07 16:28:32 -0500

    Esta es min presentación

--- Presentacion.java
+++ Presentacion.java
@@ -2,7 +2,7 @@ public class Presentacion{
     public static void main(String args[]) {
     
         //Mi presentación
-        System.out.println ("Mi nombre es Oliver Mayta :)");
+        System.out.println ("Mi nombre es Oliver Mayta Nolasco :)");
 
     }
 }
