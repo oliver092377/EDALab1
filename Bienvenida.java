c99c326f53b54ca74f6d16702e4c9c0c4cd48216
oliver092377 <omayta@unsa.edu.pe>
2022-05-07 16:27:52 -0500

    Bienvenida personalizada

--- Bienvenida.java
+++ Bienvenida.java
@@ -2,7 +2,7 @@ public class Bienvenida{
     public static void main(String args[]) {
     
         //Mi bienvenida al curso de EDA
-        System.out.println ("Bienvenido al curso de EDA");
+        System.out.println ("Bienvenido al curso de EDA(Estructuras de datos y Algoritmos)");
 
     }
 }
